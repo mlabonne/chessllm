4c7cdfa0206f646e0f7926719efe93ce0991737f
Kostis Gourgoulias <kgourgoulias@gmail.com>
2024-01-14 01:02:52 +0000

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt", "r") as f:
-    requirements = f.read().splitlines()
 
 setup(
-    name="chess_llm_playground",
+    name="chessllm",
     description="A playground for chess LLMs.",
     license="",
     author="",
-    packages=find_packages(include="chess_llm_playground"),
-    install_requires=requirements,
+    packages=find_packages(),
 )

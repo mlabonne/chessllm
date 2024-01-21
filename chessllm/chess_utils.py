774f0bab2129d1c20bda5e73f689aefe9060fcaf
ruchi <ruchi@DESKTOP-KRV2GH1>
2024-01-21 10:28:21 +0000

    Fix for Checking and ignoring Nan results also converting half to float:

--- chessllm/chess_utils.py
+++ chessllm/chess_utils.py
@@ -1,7 +1,8 @@
 import io
 import re
 from datetime import datetime
-
+# import os
+# os.add_dll_directory(r"C:\Program Files\GTK3-Runtime Win64\bin") Uncomment this line if running on windows and GTK3 is installed
 import cairosvg
 import chess
 import gistyc
